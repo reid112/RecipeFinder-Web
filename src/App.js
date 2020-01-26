8b30c47dc396a24806c15046da7857139e29beb3
reid112 <rreid112@gmail.com>
2020-01-26 07:30:20 -0600

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from 'react'
+import RecipeCard from './components/recipe-card'
 
 function App() {
   return(
     <div>
-      <h1>Recipe Finder</h1>
+      <h2>Recipies will be shown here</h2>
+      <RecipeCard title="Recipe 1"/>
+      <RecipeCard title="Recipe 2"/>
+      <RecipeCard title="Recipe 3"/>
+      <RecipeCard title="Recipe 4"/>
+      <RecipeCard title="Recipe 5"/>
     </div>
   );
 }
---
+++ src/components/recipe-card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecipeCard(props) {
+    return(
+        <div>
+            <h3>{props.title}</h3>
+        </div>
+    )
+}
+
+export default RecipeCard
